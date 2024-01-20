fdd857de2b26622ea750a598da04ff9434fe0935
pawanpdn-671 <pawanpradhanpc671@gmail.com>
2024-01-20 22:26:01 +0530

    resolving issues

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -33,7 +33,7 @@ export default buildConfig({
 	},
 	editor: slateEditor({}),
 	db: mongooseAdapter({
-		url: process.env.MONGODB_URL as string,
+		url: process.env.MONGODB_URL!,
 	}),
 	typescript: {
 		outputFile: path.resolve(__dirname, "payload-types.ts"),
